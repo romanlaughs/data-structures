8d83f300ef52692dae396dcf4f1abe0712e3ab98
Nick Cassano <romanlaughs@gmail.com>
2022-06-18 09:37:49 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
+
 };
 
 setPrototype.contains = function(item) {
+  for (i in this._storage) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var index = (this._storage.indexOf(item));
+  this._storage.splice(index, 1);
+
 };
 
 /*
